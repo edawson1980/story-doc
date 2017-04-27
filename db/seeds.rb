344b48debf7dd3b4daa98430e8a815c36d85d2ee
Erin <erinedawson@gmail.com>
2017-04-27 16:17:39 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.destroy_all
+Doc.destroy_all
+
+ken = Doc.create(name:"Kendelyn Ouellette", location:"Washington DC", gender:"female", img_url:"https://s-media-cache-ak0.pinimg.com/236x/c9/98/8f/c9988f331872ea296739881c8453213a.jpg")
+erin = Doc.create(name:"Erin Dawson", location:"Washington DC", gender:"female", img_url:"http://www.providencejournal.com/storyimage/PJ/20150618/ENTERTAINMENTLIFE/150619364/AR/0/AR-150619364.jpg")
+
+owl = Project.create(title: "Modest Owls", theme:"nature", doc: ken, img_url:"http://68.media.tumblr.com/tumblr_ltvombl6tY1qeln1go1_1280.jpg")
+jameson = Project.create(title: "Humblebrag", theme:"feelings", doc: erin, img_url:"http://pbs.twimg.com/media/CWiKkxwVEAA9pvd.jpg:large")
