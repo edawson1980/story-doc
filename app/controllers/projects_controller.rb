783167a7f499779c764694d6a4773407b69a0602
Erin <erinedawson@gmail.com>
2017-04-28 10:21:20 -0400

    complete views and controllers for Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,44 @@
 class ProjectsController < ApplicationController
 
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.create!(project_params)
+    redirect_to project_path(@project)
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    project.update!(project_params)
+
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    redirect_to projects_path
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :theme, :img_url, :doc_id)
+  end
+
 
 end
