221ed758b4d41605d8315d8e8806e8fd200cb5b4
Erin <erinedawson@gmail.com>
2017-05-02 23:15:22 -0400

    add user authorization on update and destroy for both models

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -15,7 +15,7 @@ class DocsController < ApplicationController
   end
 
   def create
-    @doc = Doc.create!(doc_params)
+    @doc = Doc.create!(doc_params.merge(doc: current_doc))
     redirect_to doc_path(@doc), notice: "Documentarian created!"
 end
 
@@ -30,15 +30,21 @@ end
 
   def update
     @doc = Doc.find(params[:id])
-    @doc.update(doc_params)
-
+    if @doc == current_doc
+      @doc.update(doc_params)
+    else
+      flash[:alert] = "Only owner can edit"
+    end
     redirect_to doc_path(@doc)
   end
 
   def destroy
     @doc = Doc.find(params[:id])
-    @doc.destroy
-
+    if @doc == current_doc
+      @doc.destroy
+    else
+      flash[:alert] = "Only owner can delete"
+    end
     redirect_to docs_path
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 Project.destroy_all
 Doc.destroy_all
 
-ken = Doc.create(name:"Kendelyn Ouellette", location:"Washington DC", gender:"female", img_url:"https://s-media-cache-ak0.pinimg.com/236x/c9/98/8f/c9988f331872ea296739881c8453213a.jpg")
-erin = Doc.create(name:"Erin Dawson", location:"Washington DC", gender:"female", img_url:"http://www.providencejournal.com/storyimage/PJ/20150618/ENTERTAINMENTLIFE/150619364/AR/0/AR-150619364.jpg")
+ken = Doc.create(name:"Kendelyn Ouellette", location:"Washington DC", gender:"female", img_url:"https://s-media-cache-ak0.pinimg.com/236x/c9/98/8f/c9988f331872ea296739881c8453213a.jpg", email:"kendelyn.ouellette@gmail.com")
+erin = Doc.create(name:"Erin Dawson", location:"Washington DC", gender:"female", img_url:"http://www.providencejournal.com/storyimage/PJ/20150618/ENTERTAINMENTLIFE/150619364/AR/0/AR-150619364.jpg", email:"erinedawson@gmail.com")
 
 owl = Project.create(title: "Modest Owls", theme:"nature", doc: ken, body: "test test test", img_url:"http://68.media.tumblr.com/tumblr_ltvombl6tY1qeln1go1_1280.jpg")
 jameson = Project.create(title: "Humblebrag", theme:"feelings", doc: erin, body: "test test test", img_url:"http://pbs.twimg.com/media/CWiKkxwVEAA9pvd.jpg:large")
