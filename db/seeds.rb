4a3a31ed040efe7a35b94c82260fcdd888d54188
Erin <erinedawson@gmail.com>
2017-06-20 16:06:04 -0400

    reseed db to add admin boolean

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Project.destroy_all
 Doc.destroy_all
 
 ken = Doc.create(name:"Kendelyn Ouellette", location:"Washington DC", gender:"female", img_url:"https://s-media-cache-ak0.pinimg.com/236x/c9/98/8f/c9988f331872ea296739881c8453213a.jpg", email:"kendelyn.ouellette@gmail.com", password: "password")
-erin = Doc.create(name:"Erin Dawson", location:"Washington DC", gender:"female", img_url:"http://www.providencejournal.com/storyimage/PJ/20150618/ENTERTAINMENTLIFE/150619364/AR/0/AR-150619364.jpg", email:"erinedawson@gmail.com", password: "password", admin:true)
+erin = Doc.create(name:"Erin Dawson", location:"Washington DC", gender:"female", img_url:"http://www.providencejournal.com/storyimage/PJ/20150618/ENTERTAINMENTLIFE/150619364/AR/0/AR-150619364.jpg", email:"erinedawson@gmail.com", password: "password", admin: true)
 rachel = Doc.create(name:"Rachel Maddow", location:"NYC", gender:"female", img_url:"https://s-media-cache-ak0.pinimg.com/736x/ba/e6/82/bae68298b3f51d13ded1c329efd6a2ed.jpg", email:"rachelm@gmail.com", password: "password")
 
 owl = Project.create(title: "Modest Owls", theme:"nature", doc: ken, body: "A wonderful serenity has taken possession of my entire soul", img_url:"http://68.media.tumblr.com/tumblr_ltvombl6tY1qeln1go1_1280.jpg")
